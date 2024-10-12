5bc05fd7404746d58b14628394ad2658e6479616
Larry <LDLB9255@GMAIL.COM>
2024-10-12 02:26:45 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,2 +1,11 @@
 const greeting = "Hello World";
 console.log(greeting);
+
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
